34c66e10999891522447e8c15c6cd0a3cbc6e898
jalgas1 <j.ungarbaev@octo.uz>
2024-02-23 18:09:55 +0500

    jetpack_1 graph

---
+++ app/src/main/java/com/octobank/jetpack_3_1/presentation/nvgraph/NavGraph.kt
@@ -0,0 +1,26 @@
+package com.octobank.jetpack_3_1.presentation.nvgraph
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.ActivityNavigator
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+
+@Composable
+fun NavGraph(
+    //startDestination: String,
+) {
+//val navController = rememberNavController()
+
+/*    HavHost(navController = navController,startDestination = startDestination){
+        navigation(
+            route = Route.AppStartNavigation.route,
+            startDestination = Route.OnBoardingScreen.route
+        ){
+            composable(
+
+            )
+        }
+    }*/
+}
